80ef7515217d0fcc6d72b922b9f73927f63edfde
Vivek <itzvivek161@gmail.com>
2026-03-23 15:19:16 +0530

    js02

---
+++ JavaScript/JS02/index.js
@@ -0,0 +1,125 @@
+// //variable kaise banye
+
+// let name="Vivek";
+// let age=25;
+// age=45;
+
+// console.log(name,age);
+
+
+
+
+// const acc=1242;
+// acc=254;
+// console.log(acc);
+
+// //old tarika
+// // var a=25;
+// // var a=65;
+// // console.log(a);
+
+// //DATA TYPES
+
+// //1.Primitive DataType
+
+// //number,string,boolean,undefined,null,bigint,symbol
+
+// //number
+// let a = 10;
+// let b = 2.36;
+// console.log(a, b);
+
+// let a=100;
+// let b=2.345;
+// console.log(a,b);
+
+
+// //string
+// let c = "Vivek IS doing Great";
+// Let  d = "Hnji!";
+// console.log(c, d);
+
+// let a="Hnji Kaise Ho ";
+// let c= "M  Bdhiya Ap bTAO!"
+// console.log(a,c);
+
+
+
+
+
+// //Boolean
+// let login = true;
+// let Logout = false;
+// console.log(login, Logout);
+
+// //undefined
+// let user;
+
+// // const p=10; //const ke case m undefined show nhi karega isme value assign karne padege
+
+// console.log(user);
+
+// //BigInt
+
+// let num = 654612491231654n;
+// console.log(num);
+
+// ///Null
+// let weather = null;
+
+// console.log(weather);
+
+// // Symbol
+// const id1 = Symbol("id");
+
+// const id2 = Symbol("id");
+
+// console.log(id1 == id2);
+// console.log(typeof id2);
+
+//2.Non-PrimitiveDataType
+//Array,object,Function
+
+//ARRAY
+//creation of array
+// let arr =[10,20,30,false, true,"VIvek"];
+// console.log(arr);
+// console.log(typeof arr);
+//Object->Key value pair is knnown as object
+
+// let user ={
+//     name:"Vivek",
+//     acc:"1245",
+//     age:18,
+//     cat:"gen"
+// }
+// console.log(user);
+// console.log(typeof user);
+
+// let s=function add(){
+//     console.log("Hello");
+
+// }
+// console.log(s);
+// console.log(typeof s);
+
+// add();
+
+//primiative datatype is immmutable
+
+// let a = 10;
+// a = 35;
+// // console.log(a);
+
+// let str = "Vivek";
+// str[0] = "D"; //->Phele Data M koi change nhi karega Kyuki wo immutable hota h aur ye uska prrof h,value rassign ho skti pr change nhi kr skte
+// str = "Dishu";
+
+// console.log(str);
+
+
+// //Non-Primitive Datatype can be changged
+// let arr=[10,20,30,4,0,45];
+// arr.push("Vivek");
+// arr[0]="Dishi";
+// console.log(arr);
