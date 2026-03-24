067e576efd0518bb3796d312e283307e8323f6d9
Vivek <itzvivek161@gmail.com>
2026-03-24 23:19:48 +0530

    js04

---
+++ JavaScript/JS04/index.js
@@ -0,0 +1,177 @@
+//Operators
+
+//Arthemetic
+
+// console.log(2+5);
+// console.log(2-5);
+// console.log(2*5);
+// console.log(2/5);
+// console.log(2%5);
+// //reaminder ke baare m btata h
+// console.log(5**2);
+
+//Assignment Operator
+// let x=20;
+// let y=10;
+// // x=x+y;
+// x+=y;
+// // y=x*y;
+
+// console.log(x);
+
+//Comparision Operator
+// let x=20;
+// let y=30;
+// console.log(x>y);
+// console.log(x<y);
+// console.log(x>=y);
+//  console.log(x<=y);
+// console.log(x==y);
+
+// let a="121abc";
+// let b=Number(a);
+
+// console.log(b);
+//NaN->Not a number
+
+//Number->String
+// let a=10;
+// let b= String(a);
+// console.log(typeof b);
+
+// console.log(typeof String(true));
+
+// console.log(Boolean(1/0));
+
+// console.log(Boolean(""));
+//Empty string ke value false ayege.
+
+// console.log(Boolean("kytr"));
+//Agar hum isme kuch bhi likhte h hamesha hi true dega.
+
+//problem of computer science
+// let a=0.1;
+// let b=0.2;
+
+// let c=a+b;
+
+// console.log(c);
+
+//1.null is loosely equal  to  indefined only
+
+// console.log(null == undefined);
+// console.log(null === undefined);
+// console.log(null == 0);
+
+//2.>,<,>==,<==(null ->number, undefined->NaN)
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null > 0);
+// console.log(null > 0);
+
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined > 0);
+// console.log(undefined > 0);
+
+// console.log("Vivek">"Dishu");
+// console.log(10>="10");
+
+//Ek koi bhi type hai, agra uskp compare krna hai toh dusro ko kise type m badlna padega
+//Dono number mein convert honge
+
+// console.log(NaN===NaN);
+
+//LOOPS
+
+//For Loop
+
+//post increment
+//post decrement:i--
+// for(let i=0;i<10;i++){
+//     console.log(i);
+
+// }
+
+//while loop
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+
+// }
+
+//Do While
+// let i=0;
+
+// do{
+//     console.log(i);
+//     i++
+
+// }while(i<10);
+
+//Condtions
+
+//if-else
+
+// let age = 15;
+// if (age >= 18) {
+//   console.log("Eligible for Vote");
+// } else {
+//   console.log("Not Eligible");
+// }
+
+//kid,young and old
+// let age=20;
+// if (age < 18) {
+//   console.log("Kid");
+// } else if (age >= 60) {
+//   console.log("old");
+// } else {
+//   console.log("Young");
+// }
+
+
+//Logical Operator
+
+//&&,||
+
+// console.log(true&&true);
+// console.log(true&&false);
+// console.log(false&&false);
+// console.log(false&&true);
+
+// console.log(true||true);
+// console.log(true||false);
+// console.log(false||false);
+// console.log(false||true);
+
+// let a="Dishu";
+// let b="Vivek";
+// let c= a&&b;
+// console.log(c);
+
+
+// let a=0;
+// let b=20;
+// console.log(a&&b);
+
+//&& :If first value is false,it will return the first value iitself
+//if first value is true,it will return second
+
+// || or
+
+// let a=10;
+// let b=20;
+// console.log(a||b);
+
+//||:If first value is true,it will return the first value iitself
+//if first value is false,it will return second
+
+// !=
+
+// console.log(5!=5);
+
+
+//Homework
+//How it works AND:&, | or
