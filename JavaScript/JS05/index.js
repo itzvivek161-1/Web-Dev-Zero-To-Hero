76b8a3adbb9b399f64a2bd99a34e9d654268048b
Vivek <itzvivek161@gmail.com>
2026-03-26 15:10:25 +0530

    js05

---
+++ JavaScript/JS05/index.js
@@ -0,0 +1,71 @@
+//Numbers
+
+// let a=10;
+// let b=2.154;
+// let c= b.toFixed(2);
+// console.log(typeof c);
+// console.log( typeof b.toExponential(2));
+
+// console.log(b.toPrecision(4));
+// console.log(b.toString());
+
+// let a= new Number(20);
+// let b= new Number(20);
+//it is use to create a number as object
+// console.log(a===b);
+
+// console.log(typeof a);
+
+// let obj1={
+//     name:"Vivek"
+// }
+
+// let obj2=obj1;
+
+// let obj2={
+//     name:"Vivek"
+// }
+
+// console.log(obj1==obj2);
+
+// let a=10;
+// let b=10;
+
+// console.log(a==b);
+
+//Non Primitive:Reference base pe comparision hota h
+//primitive:Copy by Value
+
+
+//Math is an  obejct in javascript
+// console.log(Math.abs(-4));
+// console.log(Math.PI);
+// console.log(Math.LN2);
+// console.log(Math.atanh(2));
+// console.log(Math.ceil(8.3));
+// console.log(Math.floor(8.3));
+// console.log(Math.max(8.3,458,562,40 ));
+
+// console.log(Math.random());
+//value generated from[0,1),where 0 is inculded but 1 is not included
+
+//Satebazzi ke game(0-10);
+// console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*6)+1); 
+
+//Math.floor(Math.random()*totalNumberofOutcome)+shift
+
+//15-25
+
+// console.log(Math.floor(Math.random()*11)+15);
+
+//Formula:Math.floor(Math.random()*(max-min+1))+min
+
+//OTP generate:4 digit:1000-9999
+
+// console.log(Math.floor(Math.random()*(8999+1))+1000);
+
+function random(){
+    Math.random()*10;
+}
+
