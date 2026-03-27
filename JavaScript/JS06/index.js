f7334dd3e7741d29e0134b51d6d060200d4fcf44
Vivek <itzvivek161@gmail.com>
2026-03-27 23:58:11 +0530

    js06

---
+++ JavaScript/JS06/index.js
@@ -0,0 +1,95 @@
+//String
+
+// const str1 = "Vivek";
+// const str2 = "Dishu  Saini";
+// const day =18;
+// const str3 = `Strike is coming on ${day}`;
+
+// console.log(str3);
+
+
+// const str =`Hello Coder Army`;
+// console.log(str.length);
+// console.log(str[4]);
+
+
+//touppercase
+// const a= str.toUpperCase();
+// console.log(a);
+// console.log(str.toLowerCase());
+
+
+// const str =`Hello Coder Army Coder`;
+
+// console.log(str.indexOf('Cod'));
+
+// console.log(str.lastIndexOf('Cod'));
+
+// console.log(str.includes('Cod'));
+
+//slice
+// console.log(str.slice(2,7));
+// console.log(str.slice(7));
+// console.log(str.slice(-4,-2));
+
+
+// const str =`Hello Coder Army Coder`;
+
+// console.log(str.substring(2,5));
+//substring m negative index ko markdoe=wn nhi kr skte pr slice m kr skte h
+
+// const a="Vivek";
+// const b= " Yadav";
+// const c= a+""+b;
+// // const c=`${a+ b}`
+// console.log(c);
+
+
+// const str =`Hello Coder Army Coder`;
+
+// console.log(str.replaceAll("ode",'iam'));
+
+// const user=`  Vivek  `
+// console.log(user.trim());
+
+
+// const names = "Vivek Dishu Arjun Ritika";
+// console.log(names.split(" "));
+// console.log(names.split(", "));
+
+//DATE
+
+// const now =new Date();
+
+// console.log(now);
+// console.log(now.toString());
+
+//Local time chl rha h
+// console.log(now.getDate());
+// console.log(now.getFullYear());
+// console.log(now.toISOString());
+// console.log(now.toLocaleTimeString());
+
+//Days:Mon-Sat(based 1-7)
+//Month:Jan-Dec(0 Based)
+
+//format:year,month,date,hour,second,millisecond.
+// const now =new Date(2026,2,21,14,25,125);
+// console.log(now.toLocaleTimeString());
+
+
+//Timestamp
+const now =Date.now();
+const dates =new Date(-89745121);
+// console.log(dates.toString());
+
+
+console.log(dates);
+console.log(now);
+
+// 1774087009219 :Timestamp it is in milleseconds and it is same for all the counntry in the world
+
+
+
+
+   
