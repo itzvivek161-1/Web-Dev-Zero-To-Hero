e87f66367d23cfd011ef426b5974b1a0a866dcad
Vivek <itzvivek161@gmail.com>
2026-03-28 15:39:06 +0530

    JS07

---
+++ JavaScript/JS07/index.js
@@ -0,0 +1,137 @@
+//Array
+
+// let Marks=100;
+// let Marks2=100;
+// let Marks3=100;
+// let Marks4=100;
+
+// let marks =[100,50,70,80,90,`vivek`,true,false];
+
+// console.log(marks);
+
+// let arr = [100, 50, 70, 80, 90, `vivek`, true, false];
+
+//accesing the arr on behalf of zero base index.
+// console.log(arr[5]);
+
+// console.log(typeof arr);
+// arr[1]="dishu"
+
+// console.log(arr);
+
+//push:Putting the element from the last
+
+// arr.push(90);
+// arr.push("Strike"
+// )
+// console.log(arr);
+
+//Pop:Delete the element from the last
+
+// arr.pop();
+// console.log(arr);
+
+// //Unshift:putting the elemnt from starting
+
+// arr.unshift("arjun");
+
+// // shift:removing the elemmnt from start
+
+// arr.shift()
+
+// console.log(arr);
+
+//iterating the array
+
+// let arr =[10,20,40,50,100]
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+
+// }
+
+// for(let num of arr){
+//     console.log(num);
+
+// }
+
+// let arr =[10,30,40,50];
+// let arr2=arr;
+// arr2.push(30);
+// console.log(arr);
+
+//object(non-primitive):copy by reference hote h
+//primitive:copy by value
+
+// const arr=[10,30,50,60];
+// arr=[50,500,600,12];
+// console.log(arr);
+
+// const arr=[10,30,50,60];
+// const arr2=arr.slice(2,4);
+
+// // console.log(arr);
+// const arr3=arr.splice(1,3,"Vivek",11);
+
+// console.log(arr3);
+
+// const arr =[10,30,50,70,90];
+// const arr2=["Vivek",7,true];
+// const arr4=["Dishu",12,"Hero"];
+
+// arr.push(arr2);
+// const arr3=arr.concat(arr2,arr4);
+//const arr3=[arr,arr2,arr4];
+
+//spread operatoe
+// const arr3=[...arr,...arr2,...arr4]
+
+// console.log(arr3);
+
+// const names = ["Vivek", "Dishu", "Prjapati", "Papu", "Durga", "Priyanshu"];
+
+// const name1=names.toString();
+
+// console.log(names.join("-") );
+
+// names.sort();
+// names.reverse();
+
+// console.log(names);
+
+
+// const a =[101,90,80,32,91];
+
+// const arr=[10,"vivek","Dishu",true,false]
+
+// arr.sort();
+// console.log(arr);
+
+
+// const arr =[101,90,80,32,91];
+
+//Decending Order
+// arr.sort((a,b)=>a-b);
+
+//Asscending Order
+// arr.sort((a,b)=>b-a)
+
+//10,31,40
+//a=10
+//b=31
+
+//-ve:phele a ayega fr b ayega
+//+ve:b ayega phiir a ayega
+// console.log(arr);
+
+// const arr =[101,90,80,[32,[10,25,16,91],50]];
+
+// const a=arr.flat(Infinity);
+// console.log( typeof a);
+
+
+//  console.log(arr[3][1][2]);
+
+
+//  
+
