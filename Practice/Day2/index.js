b0c11bc7d9fbea97cc12d3a541e1873ce18169c6
Vivek <itzvivek161@gmail.com>
2026-04-13 16:11:10 +0530

    practice Day 2

---
+++ Practice/Day2/index.js
@@ -0,0 +1,107 @@
+//Arthematic Operator
+
+// console.log(5+2);
+// console.log(2%5);
+// console.log(5**2);
+
+
+//Assignment Operator
+
+// let x=20;
+// let y=12;
+// // y=x+y;
+// // x+=y;
+// x*=y;
+// console.log(x);
+
+
+//Comparision Operator
+
+// let x=20;
+// let y=30;
+// console.log(x>y);
+//  console.log(x<y);
+//  console.log(x>=y);
+//  console.log(x<=y);
+//  console.log(y=x);
+// let a=30;
+// let b="30";
+// console.log(a===b);
+
+// let a="dlfj15";
+// let  b=a;
+// console.log(typeof b);
+
+
+// let a="121abc";
+// let b=Number(a);
+
+// console.log(b);
+
+//Number=>String
+// let a=10;
+// let b=String(a);
+// console.log(typeof b);
+
+// console.log(typeof String(true));
+
+// console.log(Boolean(0/0));
+
+// console.log(Boolean(""));
+
+//Problem of computer science
+
+// let a=0.1;
+// let b=0.2;
+
+// let c=a+b;
+// console.log(c);
+
+// console.log(Number (c.toFixed(2)));
+
+//1.Null is loosely equal to  undefined
+
+// console.log(null==undefined);
+// console.log( typeof( null===undefined));
+// console.log(null==0);
+
+
+// khud test kar
+// console.log(null == undefined);
+// console.log(null === undefined);
+// console.log([] == false);
+// console.log("" == 0);
+
+//2.>,<,>==,<==(null ->number, undefined->NaN)
+
+// console.log(null>=0);
+// console.log(null<=0);
+// console.log(null>0);
+// console.log(null<0);
+
+// console.log(undefined>=0);
+// console.log(undefined<=0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+
+// console.log("Vivek">"Dishu");
+// console.log(typeof(10>"10"));
+
+// console.log(NaN===NaN);
+
+
+//lopps
+//For Loop
+// for(let i=2;i<=20;i=i+2){
+//     console.log(i);
+    
+// }
+
+//2.While Loop
+let i=0;
+while(i<10){
+    console.log(i);
+    i++;
+    
+}
