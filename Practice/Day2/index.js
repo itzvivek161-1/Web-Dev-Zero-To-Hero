84e69a9ad372d280b1ec75fadffa2a0d7770d702
Vivek <itzvivek161@gmail.com>
2026-04-13 16:34:51 +0530

    Day 2 Completed

--- Practice/Day2/index.js
+++ Practice/Day2/index.js
@@ -4,7 +4,6 @@
 // console.log(2%5);
 // console.log(5**2);
 
-
 //Assignment Operator
 
 // let x=20;
@@ -13,7 +12,6 @@
 // // x+=y;
 // x*=y;
 // console.log(x);
-
 
 //Comparision Operator
 
@@ -31,7 +29,6 @@
 // let a="dlfj15";
 // let  b=a;
 // console.log(typeof b);
-
 
 // let a="121abc";
 // let b=Number(a);
@@ -65,7 +62,6 @@
 // console.log( typeof( null===undefined));
 // console.log(null==0);
 
-
 // khud test kar
 // console.log(null == undefined);
 // console.log(null === undefined);
@@ -84,24 +80,100 @@
 // console.log(undefined>0);
 // console.log(undefined<0);
 
-
 // console.log("Vivek">"Dishu");
 // console.log(typeof(10>"10"));
 
 // console.log(NaN===NaN);
 
-
 //lopps
 //For Loop
 // for(let i=2;i<=20;i=i+2){
 //     console.log(i);
-    
+
 // }
 
 //2.While Loop
-let i=0;
-while(i<10){
-    console.log(i);
-    i++;
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+
+// }
+
+//3.Do While
+// let i = 0;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+//Conditions
+// 1.if-else
+
+// let age=15;
+// if(age>=18){
+//     console.log("Eligible for Vote");
     
-}
+// }
+// else{
+//     console.log("Not Eligible ");
+    
+// }
+
+//kid,young,old
+//  let age=20;
+//  if(age<18){
+//     console.log("Kid");
+    
+//  }
+//  else if(age>=60){
+//     console.log("old");
+    
+    
+//  }
+//  else{
+//     console.log("Young");
+    
+//  }
+
+//LOgical Operator
+// &&,||
+//  console.log(true&&true);
+// console.log(true&&false);
+// console.log(false&&false);
+// console.log(false&&true);
+
+// 👉 Rule:
+
+// Pehla false mila → wahi return
+// Sab true → last value return
+
+
+//  console.log(true||true);
+//  console.log(true||false);
+//  console.log(false||false);
+//  console.log(false||true);
+// 👉 Rule:
+
+// Pehla true mila → wahi return
+// Sab false → last value
+
+// let a="Dishu";
+// let b="Vivek";
+// let c= a&&b;
+// console.log(c);
+
+// let a=0;
+// let b=20;
+// console.log(a&&b);
+
+
+// let a=10;
+// let b=20;
+// console.log(a||b);
+
+
+// !=
+
+// console.log(5!=5);
