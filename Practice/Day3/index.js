762090e8de885eedaad7a393f957699226caceac
Vivek <itzvivek161@gmail.com>
2026-04-15 18:28:35 +0530

    Nummbers Revision By doing Practice

---
+++ Practice/Day3/index.js
@@ -0,0 +1,90 @@
+//Numbers
+
+// let a=10;
+// let b=2.54;
+// let c=b.toFixed(2);
+
+// console.log(typeof c);
+
+// console.log(b.toExponential(2));
+
+// console.log( typeof b.toPrecision(4));
+
+// console.log(b.toString());
+
+// let a=new Number(20);
+// let b= new Number(20);
+//it is use to create a number as object
+// // console.log(a===b);
+
+// console.log(typeof a);
+
+// console.log(typeof b);
+
+// let obj1 = {
+//   name: "Vivek",
+// };
+
+// let obj2 = {
+//   name: "Vivek",
+// };
+// obj2 = obj1;
+// console.log(obj2 == obj1);
+
+// let a=10;
+// let b=10;
+
+// console.log(a==b);
+
+//Non Primitive:Reference base pe comparision hota h
+//primitive:Copy by Value
+
+//Math is an  obejct in javascript
+
+// console.log(typeof Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.SQRT2);
+// // console.log(Math.LN10);
+// console.log(Math.LN2);
+// console.log(Math.LOG2E);
+// console.log(Math.acosh(4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.max(10,20,50,-50));
+// console.log(Math.tan(30));
+
+// console.log(Math.random());
+//value generated from[0,1),where 0 is inculded but 1 is not included.
+// console.log((Math.floor(Math.random()*10)+1));
+
+// Satebazzi ke game(0-10);
+// console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*6)+1);
+
+// console.log(Math.floor(Math.random()*20)+1);
+
+// Math.floor(Math.random()*totalNumberofOutcome)+shift
+
+
+//15-25
+// console.log(Math.floor(Math.random()*11+15));
+
+//50-100
+// console.log(Math.floor(Math.random()*51+50));
+
+//OTP generation:4 digit 0000-9999
+
+// console.log(Math.floor(Math.random()*10000));
+
+// function random(){
+//    return Math.random()*10+1;
+    
+// }
+
+// random();
+// const hey=random();
+// console.log(hey);
+
