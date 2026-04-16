e27c8d8c6bf0bb291c1d2f842df204cc439fbc1e
Vivek <itzvivek161@gmail.com>
2026-04-16 12:42:06 +0530

    Practice Day 4

---
+++ Practice/Day4/index.js
@@ -0,0 +1,107 @@
+//Declartion of the string
+
+
+// let str1="Vivek";
+// let str2="Dishu Saini";
+
+//Ye tareeka glt nhi h pr isko prefer nhi krete h
+// console.log((str1,str2).length);
+
+
+//ye shi tarika h
+// console.log(str1.length,str2.length);
+
+// const str=`Hello Vivek ke Army`;
+// console.log(str.length);
+// console.log([str[5]]);
+
+// const str="VIVEK YADAV";
+// // console.log(str.toLocaleUpperCase());
+// console.log(str.toLocaleLowerCase());
+
+// const str=`Hello Vivek ke Army`;
+// console.log(str.indexOf("Ar"));
+// console.log(str.lastIndexOf("vivek"));
+// console.log(str.includes("hero"));
+
+//Slice:.slice(startIndex, endIndex): Extracts a section of a string and returns it as a new string.
+//it always give a new string
+// const str=`Hello Vivek ke Army`;
+// console.log(str.slice(2,7));
+// console.log(str.slice(7));
+
+// console.log(str.slice(-4,-2));
+
+//Repalce:.replace(searchValue, newValue): Finds a searchValue and replaces it with newValue.
+
+// let greeting = "hello world, hello there";
+
+// // let newGreeting=greeting.replace("hello","hi");
+// let newGreeting=greeting.replaceAll("hello","hi");
+// console.log(newGreeting);
+
+
+//  Cleaning Up Whitespace:Trim();
+
+// trim()`:** Removes whitespace from both the beginning and end of a string.
+// trimStart()` / `.trimEnd()`:** Removes whitespace from only the start or end.
+
+
+// let userInput = "   my-username@example.com   ";
+// console.log(userInput.trim());
+
+
+// let stu=`vivek , dishu ,arjun, venky, abhi`;
+// console.log(stu.split(",").map(s=>s.trim()));
+
+// let words = "The quick brown fox";
+// let word_array = words.split(" "); // ["The", "quick", "brown", "fox"]
+// console.log(word_array);
+
+
+// let letters = "abc";
+// let letter_array = letters.split(","); // ["a", "b", "c"]
+// console.log(letter_array);
+
+//Concation=>Addition 
+// let name="Vivek";
+// let age=24;
+// let message=`MY name is ${name} and I am ${age-2} years old.`
+// console.log(message);
+
+// let htmlNew=`
+// <div>
+// <p>Hello</p>
+// </div>`
+
+// console.log(htmlNew);
+
+//Date
+
+// const now= new Date();
+
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getFullYear());
+// console.log(now.toJSON());
+
+// console.log(now.toDateString());
+// console.log(now.toLocaleString());
+
+
+//  Create a Date from a Timestamp
+
+// const currentTimestamp=Date.now();
+// console.log(currentTimestamp);
+
+
+
+// Now, let's create a Date object from that number.
+// const da=new Date(1776257010039);
+// console.log(da.toLocaleString());
+
+           
+
+
+
+
